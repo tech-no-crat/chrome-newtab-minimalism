3d1d50a342a47c1546d29c2334149c035402cbec
Christos Porios <porios@yahoo.gr>
2012-04-27 22:09:44 +0300

    intial commit

---
+++ logic.js
@@ -0,0 +1,25 @@
+var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthnames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function update() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  if (hours <= 9) hours = "0" + hours;
+  if (minutes <= 9) minutes = "0" + minutes;
+  if (seconds <= 9) seconds = "0" + seconds;
+  var time = hours + ":" + minutes + ":" + seconds;
+
+  var day_name = weekdays[now.getDay()];
+  var day = now.getDate();
+  var month = monthnames[now.getMonth()];
+  var year = now.getFullYear();
+  var date = day_name + ", " + month + " " + day + ", " + year;
+  
+  
+  document.getElementById('time').innerHTML = time;
+  document.getElementById('date').innerHTML = date;
+
+  setTimeout("update()", 1000)
+}
